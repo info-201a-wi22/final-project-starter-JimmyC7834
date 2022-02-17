7cb43ace926afa56d4f757b5d34b4781e227d1f9
JimmyCheong <c7834@uw.edu>
2022-02-17 11:41:08 -0800

    fixed dataset2 us_temp

--- source/summary_info.R
+++ source/summary_info.R
@@ -1,6 +1,44 @@
 # A function that takes in a dataset and returns a list of info about it:
-summary_info <- list()
-summary_info$num_observations <- nrow(my_dataframe)
-summary_info$some_max_value <- my_dataframe %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
+rm(list = ls())
+library(dbplyr)
+
+# summerise usa disaster data
+data_1 <- data.frame(read.csv("../data/1970-2021_DISASTERS.xlsx - emdat data.csv"))
+data_2 <- data.frame(read.csv("../data/110-tavg.csv"))
+data_1 <- data_1[data_1$ISO == "USA", ]
+
+summary_info_1 <- list()
+summary_info_1$num_observations <- nrow(data_1)
+
+summary_info_1$num_disaster_type <- length(unique(data_1 %>%
+  filter(!is.na(Disaster.Subtype)) %>%
+  filter(Disaster.Subtype != "") %>%
+  pull(Disaster.Subtype)))
+
+summary_info_1$most_occurred_disaster <- tail(names(sort(table(data_1 %>%
+                                                                 filter(!is.na(Disaster.Subtype)) %>%
+                                                                 filter(Disaster.Subtype != "") %>%
+                                                                 pull(Disaster.Subtype)))), 1)
+
+summary_info_1$most_total_damage_costed <- max(data_1 %>%
+  filter(!is.na(Total.Damages...000.US..)) %>%
+  pull(Total.Damages...000.US..)
+)
+
+summary_info_1$most_total_deaths_costed_by_single_disaster <- max(data_1 %>%
+  filter(!is.na(Total.Deaths)) %>%
+  pull(Total.Deaths)
+)
+
+summary_info_1$most_total_homeless_costed_by_single_disaster <- max(data_1 %>%
+  filter(!is.na(No.Homeless)) %>%
+  pull(No.Homeless)
+)
+
+summary_info_1$average_disaster_count <- mean((data_1 %>%
+  group_by(Year) %>%
+  summarise(count = n()))$count)
+
+aggragate_info_1$disaster_count <- data_1 %>%
+  group_by(Year) %>%
+  summarise(count = n())
