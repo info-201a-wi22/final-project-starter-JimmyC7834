4d226fa49bb7a38861ea831e66b9e8e1df5f5d9c
JimmyCheong <c7834@uw.edu>
2022-02-17 18:21:48 -0800

    moved code from table.R to info.R

--- source/summary_info.R
+++ source/summary_info.R
@@ -39,6 +39,50 @@ summary_info_1$average_disaster_count <- mean((data_1 %>%
   group_by(Year) %>%
   summarise(count = n()))$count)
 
-aggragate_info_1$disaster_count <- data_1 %>%
+
+info_1_disaster_count <- data.frame(disaster_count = data.frame(data_1 %>%
   group_by(Year) %>%
-  summarise(count = n())
+  summarise(freq = n())))
+
+info_1_top_homeless <- data_1 %>%
+  filter(!is.na(No.Homeless)) %>%
+  filter(Disaster.Type != "") %>%
+  group_by(Disaster.Type) %>%
+  summarise(n_homeless = sum(No.Homeless)) %>%
+  arrange(desc(n_homeless))
+
+install.packages("areaplot")
+library(areaplot)
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library("plotly")
+
+
+#This data set is the one focusing on the disasters and it gives the years which they have taken place
+data_file <- "https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-JimmyC7834/main/data/1970-2021_DISASTERS.xlsx%20-%20emdat%20data.csv"
+disasters_data <- read.csv(data_file, stringsAsFactors = FALSE)
+
+
+# This second data specifically focuses on the temperatures of different states.
+data_file2 <- "https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-JimmyC7834/main/data/110-tavg.csv"
+us_avg_temps <- read.csv(data_file2, stringsAsFactors = FALSE)
+
+
+# Here we created a data frame that just focuses on the Unites States and looks
+# at the different locations and the after math of the disasters
+us_df <- disasters_data %>%
+  filter(Country == "United States of America (the)") %>%
+  select(Year, Seq, Disaster.Type, Location, Start.Year, Start.Month,
+         Start.Day, End.Year, End.Month, End.Day, Total.Deaths, No.Injured, No.Affected, No.Homeless,
+         Total.Affected)
+
+# Here, We used the data frame we created to just focus on the locations and
+# the specific disaster that occurred most in each location. The question we
+# want to answer.
+# *Which disaster occurred the most each year?*
+
+new_df <- us_df %>%
+  group_by(Year) %>%
+  summarise(Disaster_Type = max(Disaster.Type), Max_Seq = max(Seq))
+View(new_df)
