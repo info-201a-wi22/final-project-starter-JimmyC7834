af7563448b11f50f9d2b535048bc759913dde72e
JimmyCheong <c7834@uw.edu>
2022-02-17 17:09:56 -0800

    added stack area graph for second chart

--- source/second_chart.R
+++ source/second_chart.R
@@ -4,9 +4,11 @@ library(stringr)
 library(dplyr)
 library(ggplot2)
 library("plotly")
-disasters <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-JimmyC7834/main/data/1970-2021_DISASTERS.xlsx%20-%20emdat%20data.csv", stringsAsFactors = FALSE)
-colnames(disasters)
-View(disasters)
+disasters <- data.frame(read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-JimmyC7834/main/data/1970-2021_DISASTERS.xlsx%20-%20emdat%20data.csv", stringsAsFactors = FALSE))
+# colnames(disasters)
+# View(disasters)
+
+disasters <- disasters[disasters$ISO == "USA",]
 
 Type <- disasters %>% 
   filter(Country == "United States of America (the)") %>% 
@@ -25,10 +27,9 @@ type_group <- data %>%
   select(Type, Frequency)
 print(type_group)
 
-View(data)
+# View(data)
 
 #final <- ggplot(data, aes(x = Year, fill = Type))
-
 
 
 disaster_plot <- ggplot(type_group, aes(Frequency, Type, fill = Type)) + 
@@ -39,3 +40,25 @@ disaster_plot <- ggplot(type_group, aes(Frequency, Type, fill = Type)) +
     y = "Type of disasters",
   )
 disaster_plot
+
+
+# year_group <- year_group %>%
+#   filter(F != 0) %>%
+#   group_by(Year) %>%
+#   summarize(
+#     Drought = sum(F[Type == "Drought"]),
+#     Earthquake = sum(F[Type == "Earthquake"]),
+#     Epidemic = sum(F[Type == "Epidemic"]),
+#     Extreme_temperature = sum(F[Type == "Extreme temperature "]),
+#     Flood = sum(F[Type == "Flood"]),
+#     Landslide = sum(F[Type == "Landslide"]),
+#     Storm = sum(F[Type == "Storm"]),
+#     Volcanic_activity = sum(F[Type == "Volcanic activity"]),
+#     Wildfire = sum(F[Type == "Wildfire"])
+#   )
+
+year_group <- data.frame(table(disasters$Year, disasters$Disaster.Type))
+year_group <- data.frame(Year = year_group$Var1, Type = year_group$Var2, F = year_group$Freq)
+
+ggplot(year_group, aes(x=Year, y=F, fill=Type, group=Type)) + geom_area()
+
